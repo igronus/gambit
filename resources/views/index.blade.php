298cc6244a08b32567681295b57bf399a746a15d
Igor Pellinen <pellinen@rkmail.ru>
2019-01-12 13:59:54 +0300

    favicon, copy-paste title fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Bookapi</title>
+    <title>Gambit</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
